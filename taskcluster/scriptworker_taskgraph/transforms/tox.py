96cc432672233705ede7120c19b4abd843aa3d21
Aki Sasaki <aki@mozilla.com>
2020-06-03 17:04:17 -0700

    the rest of the tox + docker images

--- taskcluster/scriptworker_taskgraph/transforms/tox.py
+++ taskcluster/scriptworker_taskgraph/transforms/tox.py
@@ -35,6 +35,7 @@ def tasks_per_python_version(config, jobs):
             repl_dict = {"name": job["name"], "python_version": python_version}
             task["label"] = "tox-{name}-python{python_version}".format(**repl_dict)
             task['worker']['docker-image'] = _replace_string(task['worker']['docker-image'], repl_dict)
+            task['description'] = _replace_string(task['description'], repl_dict)
             task['run']['command'] = _replace_string(task['run']['command'], repl_dict)
             # XXX default files-changed? e.g. taskcluster/ .tc.yml docker
             yield task
